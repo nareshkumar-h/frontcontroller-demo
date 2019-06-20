b09f5300d626d903770cb9455fbfc41ad33a9920
Naresh Kumar H <nareshkumarh@live.com>
2019-06-20 15:55:51 -0400

    Demo

---
+++ src/com/naresh/FrontControllerServlet.java
@@ -0,0 +1,51 @@
+package com.naresh;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FrontControllerServlet
+ */
+public class FrontControllerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		String requestURI = request.getRequestURI();
+		System.out.println("RequestURI:" + requestURI);
+		
+		String contextPath = request.getContextPath();
+		System.out.println("ContextPath:" + contextPath);
+		
+		String path = requestURI.substring(contextPath.length());
+		System.out.println("path:" + path);
+
+		switch (path) {
+		case "/login.do":
+			System.out.println("LoginServlet");
+			UserController.login(request, response);
+
+			break;
+		case "/register.do":
+			System.out.println("RegisterServlet");
+			UserController.register(request, response);
+			break;
+		}
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/com/naresh/UserController.java
@@ -0,0 +1,19 @@
+package com.naresh;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class UserController {
+
+	public static void login(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		System.out.println("Login" );
+		response.getWriter().append("UserController-login");
+	}
+	
+	public static void register(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		System.out.println("register");
+		response.getWriter().append("UserController-register");
+	}
+}
